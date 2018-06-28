c463d20b10c7c97bbd6b5b1ff6d0f94cb0997696
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-06-28 19:33:59 +0200

    Modification program.js

--- program.js
+++ program.js
@@ -1,16 +1,16 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function () {
     var input = process.stdin.read();
-    if(input !== null) {
-            var instruction = input.toString().trim();
-            switch (instruction) {
-                case '/exit':
-                    process.stdout.write('Quitting app!\n');
-                    process.exit();
-                    break;
-                default:
-                    process.stderr.write('Wrong instruction!\n');
-            }
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
     }
 });
 
